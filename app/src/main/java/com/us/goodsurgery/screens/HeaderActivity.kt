c5018b18dceb7cfee8734d3457b5e37304d56b5c
isrbrepin <isrbrepin@alum.us.es>
2024-01-19 20:54:05 +0100

    MUCHO DISEÑO

--- app/src/main/java/com/us/goodsurgery/screens/HeaderActivity.kt
+++ app/src/main/java/com/us/goodsurgery/screens/HeaderActivity.kt
@@ -2,16 +2,21 @@ package com.us.goodsurgery.screens
 
 import android.os.Bundle
 import android.view.Gravity
+import android.view.MenuItem
 import android.view.WindowManager
 import android.widget.Button
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import com.google.android.material.navigation.NavigationView
 import com.us.goodsurgery.R
 
 class HeaderActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_header)
+
     }
 }
--- app/src/main/java/com/us/goodsurgery/screens/PrincipalActivity.kt
+++ app/src/main/java/com/us/goodsurgery/screens/PrincipalActivity.kt
@@ -1,18 +1,44 @@
 package com.us.goodsurgery.screens
 
+
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Gravity
+import android.view.MenuItem
 import android.view.WindowManager
 import android.widget.Button
+import android.widget.ImageButton
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import com.google.android.material.navigation.NavigationView
 import com.us.goodsurgery.R
 
-class PrincipalActivity : AppCompatActivity() {
+
+class PrincipalActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener{
+
+    private lateinit var drawerLayout: DrawerLayout
+    private lateinit var toggle: ActionBarDrawerToggle
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_principal)
+
+        // Encuentra el ImageButton por su ID
+        val drawerButton: ImageButton = findViewById(R.id.menu_drawer)
+        drawerLayout = findViewById(R.id.drawer_layout)
+
+        // Configura el click listener para abrir el drawer
+        drawerButton.setOnClickListener {
+            // Abre el drawer
+            drawerLayout.openDrawer(GravityCompat.START)
+        }
+
+
+        //-----------------------------------------------------------------------------------------
+
         val btnComenzar: Button = findViewById(R.id.btn_comenzar)
         // Configura el evento de clic del botón
         btnComenzar.setOnClickListener {
@@ -45,5 +71,27 @@ class PrincipalActivity : AppCompatActivity() {
 
             dialog.show()
         }
+
+
+    }
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return if (toggle.onOptionsItemSelected(item)) {
+            true
+        } else super.onOptionsItemSelected(item)
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        val id = item.itemId
+        if (id == R.id.nav_inicio) {
+            // Maneja el click para "Inicio"
+        } else if (id == R.id.nav_desplegable) {
+            // Maneja el click para "Patologías"
+        } else if (id == R.id.nav_acerca_de) {
+            // Maneja el click para "Acerca de"
+        }
+        // Cierra el drawer cuando se selecciona un item
+        val drawer = findViewById<DrawerLayout>(R.id.drawer_layout)
+        drawer.closeDrawer(GravityCompat.START)
+        return true
     }
 }
