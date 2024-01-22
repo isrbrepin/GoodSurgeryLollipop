2748c0aacf35338bcd7587b88f70ed08ae94a07c
isrbrepin <isrbrepin@alum.us.es>
2024-01-22 09:44:25 +0100

    Contenidos de CRecto añadidos

--- app/src/main/java/com/us/goodsurgery/screens/patologias/cirugiaabdominal/polipos/recto/preoperatorio/IngresoRectoActivity.kt
+++ app/src/main/java/com/us/goodsurgery/screens/patologias/cirugiaabdominal/polipos/recto/preoperatorio/IngresoRectoActivity.kt
@@ -3,21 +3,103 @@ package com.us.goodsurgery.screens.patologias.cirugiaabdominal.polipos.recto.pre
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Html
 import android.view.Gravity
+import android.view.View
 import android.view.WindowManager
 import android.widget.Button
 import android.widget.ImageButton
+import android.widget.TextView
 import androidx.appcompat.app.AlertDialog
+import androidx.core.content.FileProvider
+import com.us.goodsurgery.BuildConfig
 import com.us.goodsurgery.R
 import com.us.goodsurgery.screens.PrincipalActivity
+import java.io.File
+import java.io.FileOutputStream
+import java.io.IOException
+import java.io.OutputStream
 
 class IngresoRectoActivity : AppCompatActivity() {
 
     private lateinit var btnVolverAtras: ImageButton
+    private lateinit var ingreso1: TextView
+    private lateinit var ingreso2: TextView
+    private lateinit var ingreso3: TextView
+    private lateinit var btnPdf: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_ingreso_recto)
+
+        //Logica pdf
+
+        btnPdf = findViewById(R.id.btn_pdf)
+
+        btnPdf.setOnClickListener(View.OnClickListener {
+            // Nombre del PDF en la carpeta assets
+            val assetFileName = "Instrucciones para el INGRESO _ HG_HMC.pdf"
+            // Ruta de destino en el almacenamiento interno
+            val destinationPath =
+                getExternalFilesDir(null).toString() + File.separator + assetFileName
+            try {
+                // Copiar el archivo desde assets al almacenamiento interno
+                val `in` = assets.open(assetFileName)
+                val out: OutputStream = FileOutputStream(destinationPath)
+                val buffer = ByteArray(1024)
+                var read: Int
+                while (`in`.read(buffer).also { read = it } != -1) {
+                    out.write(buffer, 0, read)
+                }
+                out.flush()
+                out.close()
+                `in`.close()
+
+                // Abrir el PDF descargado
+                val file = File(destinationPath)
+                val uri = FileProvider.getUriForFile(
+                    this,
+                    BuildConfig.APPLICATION_ID + ".provider", file
+                )
+                val intent = Intent(Intent.ACTION_VIEW)
+                intent.setDataAndType(uri, "application/pdf")
+                intent.flags =
+                    Intent.FLAG_ACTIVITY_NO_HISTORY or Intent.FLAG_GRANT_READ_URI_PERMISSION
+                startActivity(intent)
+            } catch (e: IOException) {
+                e.printStackTrace()
+                // Manejar el error
+            }
+        })
+
+        //------------------------------------------------------------------------------------------
+        // Lógica para permitir que se use el texto en html para poner negritas y rayadas por ejemplo
+
+        ingreso1 = findViewById(R.id.text_ingreso1)
+
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.N) {
+            ingreso1.setText(Html.fromHtml(getString(R.string.text_ingreso1), Html.FROM_HTML_MODE_LEGACY));
+        } else {
+            ingreso1.setText(Html.fromHtml(getString(R.string.text_ingreso1)));
+        }
+
+        ingreso2 = findViewById(R.id.text_ingreso2)
+
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.N) {
+            ingreso2.setText(Html.fromHtml(getString(R.string.text_ingreso2), Html.FROM_HTML_MODE_LEGACY));
+        } else {
+            ingreso2.setText(Html.fromHtml(getString(R.string.text_ingreso2)));
+        }
+
+        ingreso3 = findViewById(R.id.text_ingreso3)
+
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.N) {
+            ingreso3.setText(Html.fromHtml(getString(R.string.text_ingreso3), Html.FROM_HTML_MODE_LEGACY));
+        } else {
+            ingreso3.setText(Html.fromHtml(getString(R.string.text_ingreso3)));
+        }
+
+        //------------------------------------------------------------------------------------------
 
         // Lógica de la Header
 
--- app/src/main/java/com/us/goodsurgery/screens/patologias/cirugiaabdominal/polipos/recto/preoperatorio/PreparacionRectoActivity.kt
+++ app/src/main/java/com/us/goodsurgery/screens/patologias/cirugiaabdominal/polipos/recto/preoperatorio/PreparacionRectoActivity.kt
@@ -3,21 +3,83 @@ package com.us.goodsurgery.screens.patologias.cirugiaabdominal.polipos.recto.pre
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Html
 import android.view.Gravity
+import android.view.View
 import android.view.WindowManager
 import android.widget.Button
 import android.widget.ImageButton
+import android.widget.TextView
 import androidx.appcompat.app.AlertDialog
+import androidx.core.content.FileProvider
+import com.us.goodsurgery.BuildConfig
 import com.us.goodsurgery.R
 import com.us.goodsurgery.screens.PrincipalActivity
+import java.io.File
+import java.io.FileOutputStream
+import java.io.IOException
+import java.io.OutputStream
 
 class PreparacionRectoActivity : AppCompatActivity() {
 
     private lateinit var btnVolverAtras: ImageButton
+    private lateinit var info_preo: TextView
+    private lateinit var btnPdf: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_preparacion_recto)
+
+        //Logica pdf
+
+        btnPdf = findViewById(R.id.btn_pdf)
+
+        btnPdf.setOnClickListener(View.OnClickListener {
+            // Nombre del PDF en la carpeta assets
+            val assetFileName = "Indicaciones preop CCR.pdf"
+            // Ruta de destino en el almacenamiento interno
+            val destinationPath =
+                getExternalFilesDir(null).toString() + File.separator + assetFileName
+            try {
+                // Copiar el archivo desde assets al almacenamiento interno
+                val `in` = assets.open(assetFileName)
+                val out: OutputStream = FileOutputStream(destinationPath)
+                val buffer = ByteArray(1024)
+                var read: Int
+                while (`in`.read(buffer).also { read = it } != -1) {
+                    out.write(buffer, 0, read)
+                }
+                out.flush()
+                out.close()
+                `in`.close()
+
+                // Abrir el PDF descargado
+                val file = File(destinationPath)
+                val uri = FileProvider.getUriForFile(
+                    this,
+                    BuildConfig.APPLICATION_ID + ".provider", file
+                )
+                val intent = Intent(Intent.ACTION_VIEW)
+                intent.setDataAndType(uri, "application/pdf")
+                intent.flags =
+                    Intent.FLAG_ACTIVITY_NO_HISTORY or Intent.FLAG_GRANT_READ_URI_PERMISSION
+                startActivity(intent)
+            } catch (e: IOException) {
+                e.printStackTrace()
+                // Manejar el error
+            }
+        })
+        //------------------------------------------------------------------------------------------
+
+        info_preo = findViewById(R.id.text_texto_preo)
+
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.N) {
+            info_preo.setText(Html.fromHtml(getString(R.string.text_texto_preo), Html.FROM_HTML_MODE_LEGACY));
+        } else {
+            info_preo.setText(Html.fromHtml(getString(R.string.text_texto_preo)));
+        }
+
+        //------------------------------------------------------------------------------------------
 
         // Lógica de la Header
 
